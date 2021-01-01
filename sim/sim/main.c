5cd018b137e6996d2a72d171abba9483139beedb
mdvoiris <mdvoiris@gmail.com>
2021-01-01 19:36:13 +0200

    updated main

--- sim/sim/main.c
+++ sim/sim/main.c
@@ -3,12 +3,99 @@
 
 Status main(int argc, char* argv[]) {
 	Status status = INVALID_STATUS_CODE;
+    
+
+    //Check for correct input argument count
+    if (argc != 1 || argc != ARG_COUNT) {
+        printf("Error - got %d arguments, expecting %d", argc, ARG_COUNT);
+        return WRONG_ARGUMENT_COUNT;
+    }
+    else if (argc == ARG_COUNT) update_args(argv);
+
+    status = init_imems();
+    if (status) return status;
+
+    status = init_main_memory();
+    if (status) return status;
 
 
+    while ((core_done[CORE0] & core_done[CORE1] & core_done[CORE2] & core_done[CORE3]) == false) {
+        if (core_done[CORE0] == false) {
+            status = core(CORE0);
+            if (status) return status;
+        }
+        if (core_done[CORE1] == false) {
+            status = core(CORE1);
+            if (status) return status;
+        }
+        if (core_done[CORE2] == false) {
+            status = core(CORE2);
+            if (status) return status;
+        }
+        if (core_done[CORE3] == false) {
+            status = core(CORE3);
+            if (status) return status;
+        }
+        cycle++;
+
+        status = cache_update();
+        if (status) return status;
+
+    }
 
 
+	return SUCCESS;
+}
+
+void update_args(char* argv[]) {
+    for (int i = 1; i < ARG_COUNT; i++) {
+        strcpy_s(args[i], _MAX_PATH, argv[i]);
+    }
+}
+
+Status init_imems() {
+    Status status = INVALID_STATUS_CODE;
+    FILE* imem_files[NUM_OF_CORES] = { NULL };
 
 
+    for (int core = 0; core < NUM_OF_CORES; core++) {
+        fopen_s(&imem_files[core], args[IMEM0 + core], "r");
+        if (imem_files[core] == NULL)
+            return FOPEN_FAIL;
 
-	return status;
+        for (int line = 0; line < IMEM_SIZE; line++) {
+            fscanf(imem_files[core], "%08X", imem[core][line]);
+            if (feof(imem_files[core])) break;
+        }
+
+        fclose(imem_files[core]);
+    }
+    
+    return SUCCESS;
+}
+
+Status init_main_memory() {
+    Status status = INVALID_STATUS_CODE;
+    FILE* memin_file = NULL;
+
+    fopen_s(&memin_file, args[MEMIN], "r");
+    if (memin_file == NULL)
+        return FOPEN_FAIL;
+
+    for (int line = 0; line < MEM_SIZE; line++) {
+        fscanf(memin_file, "%08X", main_memory[line]);
+        if (feof(memin_file)) break;
+    }
+
+    fclose(memin_file);
+
+    return SUCCESS;
+}
+
+Status core(Core core_num) {
+
+}
+
+Status cache_update() {
+
 }
--- sim/sim/main.h
+++ sim/sim/main.h
@@ -11,7 +11,6 @@
 
 
 //Defines:
-#define REQUIERED_ARGS_COUNT 28
 #define NUM_OF_CORES 4
 #define NUM_OF_REGS 16
 #define PIPE_LEN 5
@@ -23,91 +22,97 @@
 //Typedefs:
 //Enum to organize the input argv
 typedef enum _arg {
-    IMEM0 = 1,
-    IMEM1,
-    IMEM2,
-    IMEM3,
-    MEMIN,
-    MEMOUT,
-    REGOUT0,
-    REGOUT1,
-    REGOUT2,
-    REGOUT3,
-    TRACE0,
-    TRACE1,
-    TRACE2,
-    TRACE3,
-    BUSTRACE,
-    DSRAM0,
-    DSRAM1,
-    DSRAM2,
-    DSRAM3,
-    TSRAM0,
-    TSRAM1,
-    TSRAM2,
-    TSRAM3,
-    STATS0,
-    STATS1,
-    STATS2,
-    STATS3,
-    ARG_COUNT
+	IMEM0 = 1,
+	IMEM1,
+	IMEM2,
+	IMEM3,
+	MEMIN,
+	MEMOUT,
+	REGOUT0,
+	REGOUT1,
+	REGOUT2,
+	REGOUT3,
+	TRACE0,
+	TRACE1,
+	TRACE2,
+	TRACE3,
+	BUSTRACE,
+	DSRAM0,
+	DSRAM1,
+	DSRAM2,
+	DSRAM3,
+	TSRAM0,
+	TSRAM1,
+	TSRAM2,
+	TSRAM3,
+	STATS0,
+	STATS1,
+	STATS2,
+	STATS3,
+	ARG_COUNT
 } Arg;
 
 //All of the main functions return a status enum for debugging
 typedef enum _status {
-    INVALID_STATUS_CODE = -1,
-    SUCCESS = 0,
-    WRONG_ARGUMENT_COUNT,
-    FOPEN_FAIL,
-    ALLOC_FAILED,
+	INVALID_STATUS_CODE = -1,
+	SUCCESS = 0,
+	WRONG_ARGUMENT_COUNT,
+	FOPEN_FAIL,
+	ALLOC_FAILED,
 } Status;
 
 typedef enum _core {
-    CORE0,
-    CORE1,
-    CORE2,
-    CORE3
+	CORE0,
+	CORE1,
+	CORE2,
+	CORE3
 } Core;
 
 typedef enum _bus_origid {
-    CORE0 = CORE0,
-    CORE1 = CORE1,
-    CORE2 = CORE2,
-    CORE3 = CORE3,
-    MAIN_MEMORY
+	CORE0 = CORE0,
+	CORE1 = CORE1,
+	CORE2 = CORE2,
+	CORE3 = CORE3,
+	MAIN_MEMORY
 } Bus_origid;
 
 typedef enum _bus_cmd {
-    NO_COMMAND,
-    BusRd,
-    BusRdX,
-    Flush,
+	NO_COMMAND,
+	BusRd,
+	BusRdX,
+	Flush,
 } Bus_cmd;
 
 typedef struct _tsram {
-    int MSI;
-    int tag;
+	int MSI;
+	int tag;
 }TSRAM;
 
 typedef struct _bus {
-    Bus_origid origid;
-    Bus_cmd cmd;
-    int addr;
-    int data;
+	Bus_origid origid;
+	Bus_cmd cmd;
+	int addr;
+	int data;
 }BUS;
 
 typedef struct _instruction {
-    int opcode;
-    int rd;
-    int rs;
-    int rt;
-    int imm;
-    int pc;
+	int opcode;
+	int rd;
+	int rs;
+	int rt;
+	int imm;
+	int pc;
 }Instruction;
 
 
 //Global variables:
+char args[ARG_COUNT][_MAX_PATH] = { "sim.exe","imem0.txt","imem1.txt","imem2.txt","imem3.txt",
+		"memin.txt","memout.txt","regout0.txt","regout1.txt","regout2.txt","regout3.txt",
+		"core0trace.txt","core1trace.txt","core2trace.txt","core3trace.txt","bustrace.txt",
+		"dsram0.txt","dsram1.txt","dsram2.txt","dsram3.txt","tsram0.txt","tsram1.txt","tsram2.txt","tsram3.txt",
+		"stats0.txt","stats1.txt","stats2.txt","stats3.txt" };
 int cycle = 0;
+bool core_done[NUM_OF_CORES] = { false };
 int pc[NUM_OF_CORES] = { 0 };
 int dsram[NUM_OF_CORES][CACHE_SIZE] = { 0 };
 TSRAM tsram[NUM_OF_CORES][CACHE_SIZE] = { 0 };
@@ -118,6 +123,16 @@ Instruction Pipe[NUM_OF_CORES][PIPE_LEN] = { 0 };
 int imem[NUM_OF_CORES][IMEM_SIZE] = { 0 };
 
 
+//Function Handles:
+void update_args(char* argv[]);
+
+Status init_imems();
+
+Status init_main_memory();
+
+Status core(Core core_num);
+
+Status cache_update();
 
 
 #endif // __MAIN_H__
